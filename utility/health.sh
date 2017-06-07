7fe45f26def3267fd174b4c8d151051c4e5fb278
revanth <reairre@cisco.com>
2017-06-06 23:05:24 -0700

    Fix for unix encoding issue for health.sh

--- utility/health.sh
+++ utility/health.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 STATUS="/usr/local/bin/status"
+
 if [[ $(/opt/cassandra/apache-cassandra-2.2.9/bin/nodetool status | grep $(hostname -i)) == *"UN"* ]]; then
-	
 		echo "UN" > $STATUS                                                                                               
         exit 0                                                                                                                                                                                                                                                                                                                                                                                                      
 else                                                                                                                                                                                                      
--- utility/readiness.sh
+++ utility/readiness.sh
@@ -6,13 +6,13 @@ HOSTNAME=$(hostname)
 IS_HOSTNAME=$(curl http://localhost:8500/v1/catalog/service/kong-db | jq -r ".[] | select(.Node | contains(env.HOSTNAME))? | .Node")  
 CONSUL_API_EXIT=$?                                                                                                                                                                    
 
-if [[ $(/opt/cassandra/apache-cassandra-2.2.9/bin/nodetool status | grep $(hostname -i)) == *"UN"* ]]; then                                                                                               
+if [[ $(cat /usr/local/bin/status) == "UN" ]]; then                                                                                               
 		if [ $CONSUL_API_EXIT -eq 0 ]; then                                                                                                                                                   
    			if [[ $IS_HOSTNAME -ne $HOSTNAME ]]; then exit 1; fi                                                                                                                           
 		fi 
-		echo "UN" > $STATUS                                                                                               
+		echo "Node is UP"                                                                                              
         exit 0                                                                                                                                                                                                                                                                                                                                                                                                      
 else                                                                                                                                                                                                      
-        echo "DN"  > $STATUS                                                                                                                                                                              
+        echo "Node is down"                                                                                                                                                                             
         exit 1                                                                                                                                                                                            
 fi
